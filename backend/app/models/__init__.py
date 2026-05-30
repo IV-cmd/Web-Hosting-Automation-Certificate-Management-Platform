99b7c8d1747fedcb59b6bc981514af04f11a9d28
IV-cmd <kanaprilla@gmail.com>
2026-05-30 14:51:48 +0200

    Added initialization package for models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.user import User
+from app.models.application import Application
+from app.models.deployment import Deployment
+from app.models.certificate import Certificate
+
+__all__ = ['User', 'Application', 'Deployment', 'Certificate']
